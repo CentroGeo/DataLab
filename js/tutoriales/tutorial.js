d7fd6e4bc8a4ca8cdab3eff4d02410befb476b4c
LoboStark <you@example.com>
2020-02-12 11:30:07 -0600

    Menu de arriba

--- js/tutoriales/tutorial.js
+++ js/tutoriales/tutorial.js
@@ -1,5 +1,14 @@
-$( function() {
-   $( "#cursos" ).accordion({
-     collapsible: true
-   });
- } );
+var acc = document.getElementsByClassName("accordion");
+var i;
+
+for (i = 0; i < acc.length; i++) {
+  acc[i].addEventListener("click", function() {
+    this.classList.toggle("active");
+    var panel = this.nextElementSibling;
+    if (panel.style.display === "block") {
+      panel.style.display = "none";
+    } else {
+      panel.style.display = "block";
+    }
+  });
+}
